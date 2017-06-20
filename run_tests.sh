0b299dd61d03e2825af51af6e74e938b3005c667
Juergen Brendel <juergen@brendel.com>
2017-06-20 12:36:30 +1200

    Add coverage and test script.

---
+++ run_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+COVERAGE_REPORT_DIR=/tmp/vpc-router-coverage
+nosetests -v --with-coverage --cover-erase \
+          --cover-html --cover-html-dir=$COVERAGE_REPORT_DIR \
+          --cover-package utils \
+          --cover-package args \
+          --cover-package errors \
+          --cover-package http \
+          --cover-package vpc \
+          --cover-package watcher \
+          --cover-package monitor
+
+echo "@@@ Coverage report: file://$COVERAGE_REPORT_DIR/index.html"
